e9be9ef9f34974ffe764e9e657e4730af041aae1
James Bliss <james.bliss@serviscreen.com>
2014-10-31 12:19:44 -0400

    Add helper class

--- couchcushion/setuptools.py
+++ couchcushion/setuptools.py
@@ -1,11 +1,25 @@
 import setuptools.command.install
 import setuptools.command.develop
 from pprint import pprint
+from . import register_external, register_static
 
 def validate_config(dist, attr, value):
 	print("ValidateConfig:", dist, attr, value)
 
-class install(setuptools.command.install.install):
+class cdb_helper:
+	def process(self, cdb):
+		if 'statics' in cdb:
+			self.process_statics(cdb['statics'])
+
+	def process_statics(self, statics):
+		for prefix, path in statics.items():
+			# FIXME: Resolve path
+			register_static(prefix, path)
+
+	def process_daemons(self, daemons):
+		pass
+
+class install(setuptools.command.install.install, cdb_helper):
 	def initialize_options(self):
 		print("install.initialize_options")
 		return super().initialize_options()
@@ -18,7 +32,7 @@ class install(setuptools.command.install.install):
 		print("install.run")
 		return super().run()
 
-class develop(setuptools.command.develop.develop):
+class develop(setuptools.command.develop.develop, cdb_helper):
 	def initialize_options(self):
 		print("develop.initialize_options")
 		return super().initialize_options()
@@ -31,4 +45,8 @@ class develop(setuptools.command.develop.develop):
 		print("develop.run")
 		pprint(vars(self.distribution))
 		pprint(self.distribution.couchdb)
+
+		cdb = self.distribution.couchdb
+		if cdb is not None:
+			self.process(cdb)
 		return super().run()
